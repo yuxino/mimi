3fbc209969a063d4753dfb51b4d5c88433499438
yuxino <nbsawz@gmail.com>
2026-07-22 21:21:40 +0800

    feat: capture macOS system audio

--- Tests/MimiCoreTests/main.swift
+++ Tests/MimiCoreTests/main.swift
@@ -4,6 +4,7 @@ var runner = TestRunner()
 runSubtitleReducerTests(using: &runner)
 runLiveTranslateProtocolTests(using: &runner)
 runSessionControllerTests(using: &runner)
+runPCMConversionTests(using: &runner)
 
 print("\n\(runner.passed) passed, \(runner.failed) failed")
 if runner.failed > 0 {
